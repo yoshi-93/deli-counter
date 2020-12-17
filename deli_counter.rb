b686595c23b60e18cba28e849cc96955debe0593
Mario Lopez <ez0212sp@icloud.com>
2020-12-16 20:24:12 -0500

    commit tree 838b603bd8e6d653d0e0a347809ab762b107aaec

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+require 'pry'
+
+def line(deli)
+    if deli.empty?
+        puts "The line is currently empty."
+    else
+        queue = "The line is currently:"
+        deli.each.with_index(1) do |name, i|
+            queue << " #{i}. #{name}"
+        end 
+        puts queue 
+    end 
+end
+
+def take_a_number(deli, person)
+    if deli.empty?
+        deli.push(person)
+        puts "Welcome, #{person}. You are number 1 in line."
+    else
+        deli.push(person)
+        puts "Welcome, #{person}. You are number #{deli.length} in line."
+    end
+end
+
+def now_serving(deli)
+    if deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli.shift}."
+    end
+end
+
+
+        
